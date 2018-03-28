59275feb36bdb72d35eaa44bb384b31aa37141a8
huangguangda <2397923107@qq.com>
2018-03-28 21:54:52 +0800

    客户端的Activity负责生成程序界面，并为程序的按钮单击事件绑定事件监听器，当用户单击按钮时向服务器发送信息。

--- app/src/main/java/cn/edu/gdmec/android/csdemo/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/csdemo/MainActivity.java
@@ -1,35 +1,57 @@
 package cn.edu.gdmec.android.csdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private LinearLayout view;
     private TextView show;
+    private EditText input;
+    private Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
         view = (LinearLayout) findViewById(R.id.view);
         findViewById(R.id.send).setOnClickListener(this);
         show = (TextView) findViewById(R.id.show);
     }
 
-    private EditText getInput(){
+    private EditText getInput() {
         return (EditText) findViewById(R.id.input);
     }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.send:
                 break;
+        }
+    }
+
+    private void initView() {
+        input = (EditText) findViewById(R.id.input);
+        send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(this);
+    }
+
+    private void submit() {
+        // validate
+        String inputString = input.getText().toString().trim();
+        if (TextUtils.isEmpty(inputString)) {
+            Toast.makeText(this, "inputString不能为空", Toast.LENGTH_SHORT).show();
+            return;
         }
     }
 }
